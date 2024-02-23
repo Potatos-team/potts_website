ce45bbbf4b957e48fe841c2873477f7db13d629a
Samuel <osamuelhere@gmail.com>
2024-02-23 02:29:36 +0000

    Implementação da rotas e table na tela de permissões

--- src/components/content/Petition/Petition.jsx
+++ src/components/content/Petition/Petition.jsx
@@ -1,26 +1,21 @@
-import { useState } from "react";
+import CallToAction from '../CallToAction/CallToAction'
+import TablePetition from '../ReusableComponents/Table'
+
 
 export default function Petition() {
-
-
-    const InputExample = () => {
-        const [texto, setTexto] = useState('');
-
-        const handleChange = (e) => {
-            setTexto(e.target.value);
-            console.log(texto)
-        };
-    }
-
     return (
-        <div>
-            <div>
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    Não é um membro?{' '}
-                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Associe-se agora
-                    </a>
-                </p>
+        <div >
+            <CallToAction />
+            <div class="px-10">
+                <div className="my-10 lg:flex lg:items-center lg:justify-between">
+                    <div className="min-w-0 flex-1">
+                        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                            Petições
+                        </h2>
+                    </div>
+                </div>
+
+                <TablePetition />
             </div>
         </div>
     )
