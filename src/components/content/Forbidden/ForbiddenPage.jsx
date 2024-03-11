0f35c74e50df9a6a46245853aa0d5ecf0c1b9844
Fillipe Meireles <fillipemeireles@Fillipes-Air.intelbras.local>
2024-03-11 09:15:23 -0300

    forbidden page

--- src/components/content/Forbidden/ForbiddenPage.jsx
+++ src/components/content/Forbidden/ForbiddenPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-
-
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content } = Layout;
 export default function ForbiddenPage() {
     return (
-        <div >
-                forbidden
+        <>
+   
+            <Header style={{ display: 'flex', alignItems: 'center' }}>
+                <div className="demo-logo" />
+                </Header>
+        
+        <div className="h-screen flex items-center justify-center">     
+                <h1 className="italic text-xl">Você não tem créditos suficientes. </h1>
+                <p><a href="https://processosemadvogado.com.br/" className="text-blue-600"> Voltar para página principal</a></p>
         </div>
+
+        </>
     )
 }
